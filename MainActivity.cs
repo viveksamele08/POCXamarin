0666d0066ae64221ba82087ff1f4e5cc1d62644a
DREAMORBIT\faiz.anwar <Faiz>
2017-07-13 15:08:48 +0530

    as

---
+++ MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace MyDemoSP
+{
+    [Activity(Label = "MyDemoSP")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.activity_main);
+        }
+    }
+}
+

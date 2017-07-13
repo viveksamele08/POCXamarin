ccd338583ffa95c72fea9ca6b114e64480cff576
Vivek Samele <vivek.samele@dreamorbit.com>
2017-07-13 18:51:08 +0530

    main activity refacotored

--- MyDemoSP/MainActivity.cs
+++ MyDemoSP/MainActivity.cs
@@ -26,6 +26,7 @@ namespace MyDemoSP
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.activity_main);
 
+            IsPlayServicesAvailable();
 
         }
 
